1dc33b418ff2bbf974790e0ebcfcac9cd0287799
Villaflores04 <tkpaulvincent042005@gmail.com>
2026-09-09 01:13:47 +0800

    Fix emoji pulse submission payload

--- components/FeedbackForm.tsx
+++ components/FeedbackForm.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { EMOJI_META, EMOJIS, type PulseEmoji } from "@/lib/sentiment";
+import { EMOJI_META, EMOJIS, ratingFromEmoji, type PulseEmoji } from "@/lib/sentiment";
 import type { Feedback } from "@/lib/types";
 import { NameModal, ProfileChip, useProfileName } from "./ProfileGate";
 
